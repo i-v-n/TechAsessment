7916254f8c7f4dd7cefe239ac3e7782c3c69171f
i-v-n <100366220+i-v-n@users.noreply.github.com>
2022-02-28 12:30:33 +0200

    fix: code indentations as white spaces

--- Task_1/src/com/sumup/activationsquad/techassessment/ivan/UI_Automation_Task.java
+++ Task_1/src/com/sumup/activationsquad/techassessment/ivan/UI_Automation_Task.java
@@ -25,19 +25,19 @@ class UI_Automation_Task {
 	@Test
 	public void Do_stuff_on_page_() {
 		
-		/* ***************************************************************/
-		/* TEST DATA                                                     */
-		/* ***************************************************************/
-		final String URL_HELP_EN_GB = "https://help.sumup.com/en-GB";
-		final String US = "United States";
-		final String UK = "United Kingdom";
-		final String SEARCH_TERM = "rare";
-		final int EXPECTED_RESULTS = 3;
-		/* ***************************************************************/
+        /* ***************************************************************/
+        /* TEST DATA                                                     */
+        /* ***************************************************************/
+        final String URL_HELP_EN_GB = "https://help.sumup.com/en-GB";
+        final String US = "United States";
+        final String UK = "United Kingdom";
+        final String SEARCH_TERM = "rare";
+        final int EXPECTED_RESULTS = 3;
+        /* ***************************************************************/
 		
-		// instantiate needed helper classes 
-		GeneralHelper helper = new GeneralHelper();
-		HelpPageObjects helpPage = new HelpPageObjects();
+        // instantiate needed helper classes 
+        GeneralHelper helper = new GeneralHelper();
+        HelpPageObjects helpPage = new HelpPageObjects();
 		
         // open a Chrome browser and maximize it
         driver = new ChromeDriver(options);
@@ -54,7 +54,7 @@ class UI_Automation_Task {
         helper.waitForSeconds(5);
         
         /*
-         * b. Using‌‌ the‌‌ country‌‌ drop-down‌‌ on‌‌ the‌‌ bottom‌‌ of the page, navigate to "United States"
+         * b. Using the country drop-down on the bottom of the page, navigate to "United States"
          */
         
         // Get the country selection element with text "United Kingdom"
@@ -110,30 +110,30 @@ class UI_Automation_Task {
 	}
 
 	@BeforeAll
-    public static void setup() throws Exception {
+        public static void setup() throws Exception {
 		
-		// Set the path to the chrome driver executable, depending on the operating system
-		String os = System.getProperty("os.name").toLowerCase();
-		String chromeDriverBin = null;
-		if (os.contains("win")){
-		    //Operating system is based on Windows
-			chromeDriverBin  = "chromedriver.exe";
+        // Set the path to the chrome driver executable, depending on the operating system
+        String os = System.getProperty("os.name").toLowerCase();
+        String chromeDriverBin = null;
+        if (os.contains("win")){
+                //Operating system is based on Windows
+                chromeDriverBin  = "chromedriver.exe";
 		}
 		else if (os.contains("nix") || os.contains("nux")){
-		    //Operating system is based on Linux/Unix
-			chromeDriverBin = "chromedriver";
+                //Operating system is based on Linux/Unix
+                chromeDriverBin = "chromedriver";
 		} else {
-			throw new Exception("Unsupported operating system! Only Windows and Linux supported");
+                throw new Exception("Unsupported operating system! Only Windows and Linux supported");
 		}
 		
-		Path currentPath = Paths.get(System.getProperty("user.dir"));
-		Path filePath = Paths.get(currentPath.toString(), "chromeDriver", chromeDriverBin);
-		System.setProperty("webdriver.chrome.driver", filePath.toAbsolutePath().toString());
+        Path currentPath = Paths.get(System.getProperty("user.dir"));
+        Path filePath = Paths.get(currentPath.toString(), "chromeDriver", chromeDriverBin);
+        System.setProperty("webdriver.chrome.driver", filePath.toAbsolutePath().toString());
 		
         // initialize chrome driver
-		options = new ChromeOptions();
+        options = new ChromeOptions();
 		
-		// This option prevents the SumUp page from presenting bot challenges (CAPTCHAs)
+        // This option prevents the SumUp page from presenting bot challenges (CAPTCHAs)
         options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
 	}
 
